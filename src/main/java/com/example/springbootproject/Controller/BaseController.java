bc30cddf51787935a0ae0b2fd817a0e83380d351
01 <1305862957@qq.com>
2024-10-19 16:43:25 +0800

    init

--- src/main/java/com/example/springbootproject/Controller/BaseController.java
+++ src/main/java/com/example/springbootproject/Controller/BaseController.java
@@ -2,13 +2,14 @@ package com.example.springbootproject.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BaseController {
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello World";
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @ResponseBody public String hello() {
+        return "Hello 新华学院！！！";
     }
+
 }
